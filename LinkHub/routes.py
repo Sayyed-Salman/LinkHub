3696942e089a196dfb7e4c649bd7b51295ed0c6c
Sayyed-Salman <sayyedsalman2000@gmail.com>
2023-02-02 22:21:52 +0530

    Update Add Resource form

--- LinkHub/routes.py
+++ LinkHub/routes.py
@@ -3,6 +3,39 @@ from flask import render_template
 home_blueprint = Blueprint('home', __name__)
 new_blueprint = Blueprint('new', __name__, url_prefix="/projects")
 
+data = [
+    {
+        "name": "Ben Ten",
+        "description": "IR Practical 3 Code",
+        "link": "https://www.google.com"
+    },
+    {
+        "name": "Mike Oxlong",
+        "description": "How to make pancakes.",
+        "link": "https://www.google.com"
+    },
+    {
+        "name": "Dicks Dinker",
+        "description": "How to grow your plants an inch longer ;)",
+        "link": "https://www.google.com"
+    },
+    {
+        "name": "Blob Biter",
+        "description": "The only article you will need.",
+        "link": "https://www.google.com"
+    },
+    {
+        "name": "Tren Bolate",
+        "description": "Gains max.",
+        "link": "https://www.google.com"
+    },
+    {
+        "name": "Mein Kamph",
+        "description": "How to reach the golden number (7mil.)",
+        "link": "https://www.google.com"
+    },
+]
+
 
 @home_blueprint.route("/")
 def index_sus():
@@ -11,4 +44,9 @@ def index_sus():
 
 @home_blueprint.route("/testserver")
 def test():
-    return render_template('base.html')
+    return render_template('base.html', data=data)
+
+
+@home_blueprint.route("/addresource")
+def add_resource():
+    return render_template('addresource.html')
--- config.py
+++ config.py
@@ -6,6 +6,5 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 class Config():
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or "very secret key xuxu"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
-        "sqlite:///" + os.path.join(base_dir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(base_dir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
